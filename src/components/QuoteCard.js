947de179649188c504dcee8147fb89c9b11313a5
github username <github email address>
2018-10-12 17:43:52 -0400

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes'
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
-      </div>
-    </div>
-  </div>;
+const QuoteCard = ( { quote: { id, content, author, votes }, removeQuote, upvoteQuote, downvoteQuote }) =>
+	<div>
+		<div className="card card-inverse card-success card-primary mb-3 text-center">
+			<div className="card-block">
+				<blockquote className="card-blockquote">
+					<p>{content}</p>
+					<footer>- author <cite title="Source Title">{author}</cite></footer>
+				</blockquote>
+			</div>
+			<div className="float-right">
+				<div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+					<button
+						type="button"
+						className="btn btn-primary"
+						onClick={() => upvoteQuote(id)}
+					>
+						Upvote
+					</button>
+					<button
+						type="button"
+						className="btn btn-secondary"
+						onClick={() => downvoteQuote(id)}
+					>
+						Downvote
+					</button>
+					<button
+						type="button"
+						className="btn btn-danger"
+						onClick={() => removeQuote(id)}
+					>
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div>Votes: {votes}</div>
+			</div>
+		</div>
+	</div>;
 
-export default QuoteCard;
+export default connect(null, { removeQuote, upvoteQuote, downvoteQuote })(QuoteCard);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,18 @@
 export default (state = [], action) => {
-  return state;
+	switch (action.type) {
+		case 'ADD_QUOTE':
+			return [...state, action.quote]
+		case 'REMOVE_QUOTE':
+			return state.filter(q => q.id !== action.quoteId)
+		case 'UPVOTE_QUOTE':
+			const quoteToBeUpvoted = {...state.find(q => q.id === action.quoteId)}
+			const otherQuotes1 = state.filter(q => q.id !== action.quoteId)
+			return [...otherQuotes1, { ...quoteToBeUpvoted, votes: ++quoteToBeUpvoted.votes }]
+		case 'DOWNVOTE_QUOTE':
+			const quoteToBeDownvoted = {...state.find(q => q.id === action.quoteId)}
+			const otherQuotes2 = state.filter(q => q.id !== action.quoteId)
+			return quoteToBeDownvoted.votes ? [...otherQuotes2, { ...quoteToBeDownvoted, votes: --quoteToBeDownvoted.votes }] : state
+		default:
+			return state
+	}
 }
